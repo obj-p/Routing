8c1b6fb9b0d65921b4150d31e3d2199c36ca67c7
Jason Prasad <jwalapr@gmail.com>
2015-09-28 08:26:23 -0400

    JJP: adding Routing

---
+++ Routing/Routing.swift
@@ -0,0 +1,18 @@
+//
+//  Routing.swift
+//  Routing
+//
+//  Created by Jason Prasad on 9/28/15.
+//  Copyright © 2015 Routing. All rights reserved.
+//
+
+import Foundation
+
+public class Routing {
+    
+    public init() {}
+    
+    public func route() -> Void {}
+    
+    public func open() -> Void {}
+}
