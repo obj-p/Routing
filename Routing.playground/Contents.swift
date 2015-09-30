97246d40124289a22e505e549b9eba9bfd986383
Jason Prasad <jwalapr@gmail.com>
2015-09-30 14:14:08 -0400

    JJP: folding parameter matcher into routematcher

--- Routing.playground/Contents.swift
+++ Routing.playground/Contents.swift
@@ -5,11 +5,19 @@ let string = NSURLComponents(URL: NSURL(string:"http://google/api/:id/:foo")!, r
     ?? ""
 
 let route = "/route/one/:id/:foo"
-var regex = "^\(route)/?$"
+var regex: String! = "^\(route)/?$"
 
-let parameters = (try? NSRegularExpression(pattern: ":[a-zA-Z0-9-_]+", options: .CaseInsensitive))
+let ranges = (try? NSRegularExpression(pattern: ":[a-zA-Z0-9-_]+", options: .CaseInsensitive))
     .map { $0.matchesInString(regex, options: [], range: NSMakeRange(0, regex.characters.count)) }?
     .map { $0.range }
+
+let parameters = ranges?
     .map { (regex as NSString).substringWithRange($0) }
 
+let keys = parameters?
+    .map { $0.stringByReplacingOccurrencesOfString(":", withString: "") }
 
+regex = parameters?
+    .reduce(regex) { $0.stringByReplacingOccurrencesOfString($1, withString: "([^/]+)") }
+
+print(regex)
--- Routing/Routing.swift
+++ Routing/Routing.swift
@@ -10,26 +10,24 @@ import Foundation
 
 public class Routing {
     public typealias RouteHandler = (parameters: [String : String]) -> Void
-    public typealias RouteMatcher = (String) -> RouteHandler?
-    public typealias ParameterMatcher = (String) -> [String : String]?
+    public typealias RouteMatcher = (String) -> (RouteHandler?, [String : String]?)
     
-    public var matchers: [(RouteMatcher, ParameterMatcher)] = [(RouteMatcher, ParameterMatcher)]()
+    public var matchers: [RouteMatcher] = [RouteMatcher]()
     
     public init() {}
     
     public func route(matcher: String, handler: RouteHandler) -> Void {
-        let _ = regex(matcher)
-        
-        let rm = { (string: String) -> RouteHandler? in return nil }
-        let pm = { (string: String) -> [String : String]? in return nil }
-        self.matchers.append((rm, pm))
+//        let (_, _) = matchers(matcher)
+//        let rm = { (String) -> RouteMatcher? in return nil }
+//        
+//        self.matchers.append(rm)
     }
     
     public func open(URL: NSURL) -> Bool {
         let route = NSURLComponents(URL: URL, resolvingAgainstBaseURL: false)
             .map { "/" + ($0.host ?? "") + ($0.path ?? "") }
         
-        if let matched = route.map({ (route) -> [(RouteHandler?, [String : String]?)] in self.matchers.map { ($0.0(route), $0.1(route)) } }) {
+        if let matched = route.map({ (route) -> [(RouteHandler?, [String : String]?)] in self.matchers.map { $0(route) } }) {
             for case (let handler, let parameters) in matched where handler != nil { handler!(parameters: parameters ?? [:]) }
             return true
         }
@@ -40,11 +38,23 @@ public class Routing {
     static let urlParameterPattern = "([^/]+)"
     let urlParameter: NSRegularExpression = try! NSRegularExpression(pattern: urlParameterPattern, options: .CaseInsensitive)
     
-    func regex(string: String) -> String? {
-        let _ = (try? NSRegularExpression(pattern: ":[a-zA-Z0-9-_]+", options: .CaseInsensitive))
-            .map { $0.matchesInString("^\(string)/?$", options: [], range: NSMakeRange(0, string.characters.count))}
+    func matchers(string: String) -> (regex: String?, keys: [String]?) {
+        var regex: String! = "^\(route)/?$"
         
-        return nil
+        let ranges = (try? NSRegularExpression(pattern: ":[a-zA-Z0-9-_]+", options: .CaseInsensitive))
+            .map { $0.matchesInString(regex, options: [], range: NSMakeRange(0, regex.characters.count)) }?
+            .map { $0.range }
+        
+        let parameters = ranges?
+            .map { (regex as NSString).substringWithRange($0) }
+        
+        let keys = parameters?
+            .map { $0.stringByReplacingOccurrencesOfString(":", withString: "") }
+        
+        regex = parameters?
+            .reduce(regex) { $0.stringByReplacingOccurrencesOfString($1, withString: "([^/]+)") }
+        
+        return (regex, keys)
     }
     
 }
