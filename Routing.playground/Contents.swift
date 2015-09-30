17e218f4928d5275fb96a024f862a77b7437c475
Jason Prasad <jwalapr@gmail.com>
2015-09-29 22:32:47 -0400

    JJP: adding open(URL)

--- Routing.playground/Contents.swift
+++ Routing.playground/Contents.swift
@@ -1,2 +1,4 @@
-import Routing
+import UIKit
 
+NSURLComponents(URL: NSURL(string:"http://google/api/v1")!, resolvingAgainstBaseURL: false)
+    .map { "/" + ($0.host ?? "") + ($0.path ?? "") }
--- Routing/Routing.swift
+++ Routing/Routing.swift
@@ -8,11 +8,30 @@
 
 import Foundation
 
-public class Routing {
+public struct Routing {
+    public typealias RouteHandler = (parameters: [String : String]) -> Void
+    public typealias RouteMatcher = (String) -> RouteHandler?
+    public typealias VariableMatcher = (String) -> [String : String]?
+    
+    public var matchers: [(RouteMatcher, VariableMatcher)] = [(RouteMatcher, VariableMatcher)]()
     
     public init() {}
     
-    public func route() -> Void {}
+    public func route(matcher: String, handler: RouteHandler...) -> Void {
+        
+    }
     
-    public func open() -> Void {}
+    public func open(URL: NSURL) -> Bool {
+        let route = NSURLComponents(URL: URL, resolvingAgainstBaseURL: false)
+            .map { "/" + ($0.host ?? "") + ($0.path ?? "") }
+        
+        let matched = route.map { (route) -> [(RouteHandler?, [String : String]?)] in self.matchers.map { ($0.0(route), $0.1(route)) } }?
+            .filter { $0.0 != nil }
+        if matched == nil { return false }
+        
+        for case (let handler, let parameters) in matched! { handler!(parameters: parameters ?? [:]) }
+        return true
+    }
 }
+
+
