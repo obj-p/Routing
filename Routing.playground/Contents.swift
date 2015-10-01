61e1a5154dbf6ae6ed3961fa55e00f8cb0c4221a
Jason Prasad <jwalapr@gmail.com>
2015-10-01 08:11:38 -0400

    JJP: minimizing

--- Routing.playground/Contents.swift
+++ Routing.playground/Contents.swift
@@ -2,9 +2,7 @@ import Routing
 
 let router = Routing()
 
-router.add("/route/one/:id") { (parameters: [String : String]) in
-    print(parameters)
-}
+router.add("/route/one/:id") { (parameters: [String : String]) in }
 
 router.open(NSURL(string: "/route/one/1")!)
 router.open(NSURL(string: "/unsupported/route")!)
