8f1c8151cca7cfaddd97e4c5e67d2830486d10da
Jason Prasad <jwalapr@gmail.com>
2015-10-01 08:17:45 -0400

    JJP: including proxy

---
+++ Routing/Proxy.swift
@@ -0,0 +1,13 @@
+//
+//  Proxy.swift
+//  Routing
+//
+//  Created by Jason Prasad on 10/1/15.
+//  Copyright © 2015 Routing. All rights reserved.
+//
+
+import Foundation
+
+extension Routing {
+    
+}
