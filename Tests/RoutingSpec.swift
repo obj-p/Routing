20fca6e89b1ad69fefbbfeff456398f911137020
Jason Prasad <jwalapr@gmail.com>
2016-01-25 21:58:22 -0500

    JJP: opening proxy context

--- Tests/RoutingSpec.swift
+++ Tests/RoutingSpec.swift
@@ -58,6 +58,10 @@ class RoutingSpec: QuickSpec {
                 
             }
             
+            context("#proxy") {
+                
+            }
+            
         }
         
     }
