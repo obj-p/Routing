c04cf0221de2d36e13f5a564dc59bf888c831c17
Jason Prasad <jwalapr@gmail.com>
2016-01-28 08:33:42 -0500

    JJP: editting comment

--- Source/Routing.swift
+++ Source/Routing.swift
@@ -25,7 +25,7 @@ public class Routing {
     
     public init() {}
     
-    // TODO: make these write operatious barrier asyncs to prevent an open URL from executing while updating routes.
+    // TODO: make these write operations asyncs to prevent an open URL from executing while updating routes.
     public func proxy(pattern: String, handler: ProxyHandler) -> Void { self.routes.append(.Proxy(self.matcher(pattern, handler: handler))) }
     public func map(pattern: String, handler: RouteHandler) -> Void { self.routes.append(.Route(self.matcher(pattern, handler: handler))) }
     
